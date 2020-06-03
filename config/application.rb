e51c4329af06613d47d8f79a58b2602a5702030b
ShashKing <Shashankk165@gmail.com>
2020-06-03 18:54:43 +0530

    Cors problem solved

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module TestBack
 
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins '*' 
         resource '*', headers: :any, methods: [:get, :post, :patch, :put]
       end
     end
